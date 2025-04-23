fd76c886f0d3afffe40ac50d647e06c020244c72
Oleksandr Holopapa <sasha.klever12@gmail.com>
2025-04-23 17:36:49 +0300

    created class Island created interface Creature

---
+++ src/main/java/org/javarush/Creature.java
@@ -0,0 +1,4 @@
+package org.javarush;
+
+public interface Creature {
+}
---
+++ src/main/java/org/javarush/Island.java
@@ -0,0 +1,17 @@
+package org.javarush;
+
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Map;
+
+public class Island {
+    Map<Coordinates, Map<Type, List<Creature>>> gameField;
+
+
+
+
+
+    class Coordinates{
+
+    }
+}
