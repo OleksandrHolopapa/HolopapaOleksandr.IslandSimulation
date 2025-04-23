ceae8ca09db08cebfe6ec1f6110f2796a689900f
Oleksandr Holopapa <sasha.klever12@gmail.com>
2025-04-23 18:18:20 +0300

    created class Coordinates created enum Creature Island changed

--- src/main/java/org/javarush/Island.java
+++ src/main/java/org/javarush/Island.java
@@ -3,15 +3,10 @@ package org.javarush;
 import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class Island {
     Map<Coordinates, Map<Type, List<Creature>>> gameField;
 
 
-
-
-
-    class Coordinates{
-
-    }
 }
